84b120cd90a5061b87e01b9eea4772fb63fbe46f
Frédéric Menou <frederic.menou@gmail.com>
2023-10-12 15:51:37 +0200

    Hooks: useIdle

--- src/Mantine/Core/Hooks.purs
+++ src/Mantine/Core/Hooks.purs
@@ -1,7 +1,9 @@
 module Mantine.Core.Hooks
-  ( module Mantine.Core.Hooks.Theming
+  ( module Mantine.Core.Hooks.StateManagement
+  , module Mantine.Core.Hooks.Theming
   , module Mantine.Core.Hooks.UIDom
   ) where
 
+import Mantine.Core.Hooks.StateManagement (UseIdle, useIdle)
 import Mantine.Core.Hooks.Theming (useMantineColorScheme, MantineColorScheme (..), UseMantineColorScheme)
-import Mantine.Core.Hooks.UIDom
+import Mantine.Core.Hooks.UIDom (UseFocusWithin, UseFullscreen, UseFullscreenResult, UseMediaQuery, UseMediaQueryOptions, UseMouse, UseMouseOptions, UseMouseResult, UseMove, UseMoveHandlers, UseMovePosition, UseViewportSize, ViewportDimensions, useFocusWithin, useFullscreen, useMediaQuery, useMouse, useMouse_, useMove, useViewportSize)
---
+++ src/Mantine/Core/Hooks/StateManagement.purs
@@ -0,0 +1,14 @@
+module Mantine.Core.Hooks.StateManagement
+  ( useIdle
+  , UseIdle
+  ) where
+
+import Effect.Uncurried (EffectFn1, runEffectFn1)
+import React.Basic.Hooks (Hook, unsafeHook)
+
+foreign import useIdleImpl :: EffectFn1 Number Boolean
+
+foreign import data UseIdle :: Type -> Type
+
+useIdle :: Number -> Hook UseIdle Boolean
+useIdle n = unsafeHook (runEffectFn1 useIdleImpl n)
